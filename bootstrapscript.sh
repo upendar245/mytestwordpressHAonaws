48d9814a10f1169f53bb24ae12b7161a444544b0
upendar245 <upendar245@users.noreply.github.com>
2020-07-04 19:48:49 +0530

    Update bootstrapscript.sh

--- bootstrapscript.sh
+++ bootstrapscript.sh
@@ -2,6 +2,7 @@
 yum update -y
 yum install httpd -y
 service httpd start 
+chkconfig httpd on
 cd /var/www/html
 curl https://wordpress.org/wordpress-4.9.5.tar.gz -o wordpress.tar.gz
 tar -xvzf wordpress.tar.gz
